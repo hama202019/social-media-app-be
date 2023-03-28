89cb70a059898716c00b7867a246c7e02a68d1cf
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-28 11:42:26 +0300

    connection to mongoDB atlas is set

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json({limit: '30mb'}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => console.log("the server is up, listening on port ",process.env.PORT))
+    })
