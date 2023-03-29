135dde3af9300f0535be2ff2985029ac6c38bff4
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-29 13:00:37 +0300

    get timeline posts function is implemented

--- Routes/postRoute.js
+++ Routes/postRoute.js
@@ -1,14 +1,15 @@
 import express from "express";
-import { createPost, deletePost, getPost, likePost, updatePost } from "../Controllers/postController.js";
+import { createPost, deletePost, getPost, getTimelinePosts, likePost, updatePost } from "../Controllers/postController.js";
 const router = express.Router();
 
 router.route('/')
-    .post(createPost)
+    .post(createPost);
 router.route('/:id')
     .get(getPost)
     .put(updatePost)
     .delete(deletePost);
 router.route('/like/:id')
-    .put(likePost)
+    .put(likePost);
+router.get("/timeline/:id", getTimelinePosts);
 
 export default router;
