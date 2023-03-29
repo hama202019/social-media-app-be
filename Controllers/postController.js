a6e24612968b7d34f5654d1a36c76d82e6f43eeb
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-29 12:25:12 +0300

    update post function is implemented

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -2,8 +2,8 @@ import PostModel from "../Models/postModel.js";
 
 export const createPost = async (req, res) => {
   try {
-    await PostModel.create(req.body);
-    res.status(200).json("Post created!");
+    const newPost = await PostModel.create(req.body);
+    res.status(200).json(newPost);
   } catch (error) {
     res.status(400).json(error);
   }
@@ -17,5 +17,22 @@ export const getPost = async (req, res) => {
       res.status(200).json(post);
     } catch (error) {
       res.status(400).json(error);
+    }
+};
+
+export const updatePost = async (req, res) => {
+    const postId = req.params.id;
+    const { userId } = req.body;
+  
+    try {
+      const post = await PostModel.findById(postId);
+      if (post.userId === userId) {
+        const updatedPost = await PostModel.findByIdAndUpdate(postId, req.body, {new: true});
+        res.status(200).json(updatedPost);
+      } else {
+        res.status(403).json("Action forbidden");
+      }
+    } catch (error) {
+      res.status(500).json(error);
     }
 };
--- Routes/postRoute.js
+++ Routes/postRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost, getPost } from "../Controllers/postController.js";
+import { createPost, getPost, updatePost } from "../Controllers/postController.js";
 const router = express.Router();
 
 router.route('/')
     .post(createPost)
 router.route('/:id')
     .get(getPost)
+    .put(updatePost)
 
 export default router;
