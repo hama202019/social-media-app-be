3e71226586accbe9c1434427b3d7aaa006f8cd88
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-04 17:10:33 +0300

    a static method to check if there is a chat between two users  is created

--- Models/ChatModel.js
+++ Models/ChatModel.js
@@ -4,5 +4,9 @@ const chatSchema = mongoose.Schema({
     members: []
 }, {timestamps: true})
 
+chatSchema.statics.findChatByUsers = async function(user1Id, user2Id) {
+    return await this.findOne({ members: { $all: [user1Id, user2Id] } });
+};
+
 const ChatModel = mongoose.model("Chat", chatSchema)
 export default ChatModel
