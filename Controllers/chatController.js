71ed8fb5d54d5fd1ebd40fe57f3a2f3337a19f8f
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-04 17:20:23 +0300

    get user chats api is created

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -1,11 +1,22 @@
 import ChatModel from "../Models/ChatModel.js";
 
 export const createChat = async (req, res) => {
-    const {firstMember, secondMember} = req.body;
+    const {user1Id, user2Id} = req.body;
     try {
-        const chat = await ChatModel.create({members: [firstMember, secondMember]});
+        if(ChatModel.findChatByUsers(user1Id, user2Id)) return res.status(400).json({error: "The Chat is already exist"});
+        const chat = await ChatModel.create({members: [user1Id, user2Id]});
         res.status(200).json(chat);
     } catch (error) {
         res.status(400).json({error: error.message});
+    }
+}
+
+export const userChats = async (req, res) => {
+    const {userId} = req.params;
+    try {
+        const chats = await ChatModel.find({members: {$in: [userId]}})
+        res.status(200).json(chats)
+    } catch (error) {
+        res.status(400).json({error: error.message})
     }
 }
--- Routes/chatRoute.js
+++ Routes/chatRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createChat } from "../Controllers/chatController.js";
+import { createChat, userChats } from "../Controllers/chatController.js";
 
 const router = express.Router()
 
 router.post('/', createChat);
+router.route('/:userId', userChats);
 
 export default router
