2d87806a03b2249cc1569df6fc2182a7154c3c6f
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-04 17:50:23 +0300

    send message api is created

--- Controllers/messageController.js
+++ Controllers/messageController.js
@@ -1,1 +1,12 @@
 import MessageModel from "../Models/MessageModel.js";
+
+export const sendMessage = async (req, res) => {
+    const {chatId, senderId, messageContent} = req.body;
+    if(!chatId || !senderId || messageContent === '') return res.status(400).json({error: "some required data are missing"});
+    try {
+        const message = await MessageModel.create({chatId, senderId, messageContent});
+        res.status(200).json(message);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
