00d96f3b9d6dad1801b7af74fc6ac14c2b884ae7
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-06 15:02:09 +0300

    set socket.io connection with our server

--- server.js
+++ server.js
@@ -7,18 +7,26 @@ import userRouter from './Routes/userRoute.js';
 import postRouter from './Routes/postRoute.js';
 import chatRouter from './Routes/chatRoute.js';
 import messageRouter from './Routes/messageRoute.js';
+import {createServer} from 'http'
+import { Server } from 'socket.io';
 import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+const httpServer = createServer(app);
+export const io = new Server(httpServer, {
+    cors: {
+        origin: '*'
+    }
+})
 app.use(bodyParser.json({limit: '30mb'}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(process.env.PORT, () => console.log("the server is up, listening on port ",process.env.PORT))
+        httpServer.listen(process.env.PORT, () => console.log("the server is up, listening on port ",process.env.PORT))
     }).catch(e => console.log(e));
 
 app.use("/auth", authRouter);
